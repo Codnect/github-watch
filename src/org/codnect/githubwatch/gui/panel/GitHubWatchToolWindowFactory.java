a133721eb9b0440a6e7259bed6ac65d9f0e29241
Burak Koken <burakkokenn@gmail.com>
2019-05-11 23:57:18 +0200

    Added the tool window for GitHub Watch. And it will be updater later.

---
+++ src/org/codnect/githubwatch/gui/panel/GitHubWatchToolWindowFactory.java
@@ -0,0 +1,18 @@
+package org.codnect.githubwatch.gui.panel;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Created by Burak Köken on 4.5.2019.
+ */
+public class GitHubWatchToolWindowFactory implements ToolWindowFactory {
+
+    @Override
+    public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+
+    }
+
+}
